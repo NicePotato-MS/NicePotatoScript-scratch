c22cd4186be7e6a904d67ea20706ded26ac3e02d
NicePotato-MS <micahsuessgithub@gmail.com>
2023-04-30 03:23:42 -0500

    I want die, I forgorr looks :skull:

--- src/ScratchBlocksText/lexer.lua
+++ src/ScratchBlocksText/lexer.lua
@@ -15,14 +15,61 @@ local function lexer(script)
     local literal = {}
     local def = {}
     script = script:gsub("\t","    ")
-    local opcodeBuffer = ""
+    local mode = "normal"
+    local buffer = ""
     local pointer = 1
     for i = 1, #script do
         local char = script:sub(i, i)
-        print(char)
+        if mode == "normal" then
+            if char == "(" then
+                clearBuffer()
+                table.insert(literal, "(")
+                table.insert(def, "openReporter")
+            elseif char == ")" then
+                clearBuffer()
+                table.insert(literal, ")")
+                table.insert(def, "closeReporter")
+            elseif char == "[" then
+                clearBuffer()
+                table.insert(literal, "[")
+                table.insert(def, "openMenu")
+            elseif char == "]" then
+                clearBuffer()
+                table.insert(literal, "]")
+                table.insert(def, "closeMenu")
+            elseif char == "{" then
+                clearBuffer()
+                table.insert(literal, "{")
+                table.insert(def, "openSub")
+            elseif char == "}" then
+                clearBuffer()
+                table.insert(literal, "}")
+                table.insert(def, "closeSub")
+            elseif char == "<" then
+                clearBuffer()
+                table.insert(literal, "<")
+                table.insert(def, "openBool")
+            elseif char == ">" then
+                clearBuffer()
+                table.insert(literal, ">")
+                table.insert(def, "closeBool")
+            elseif char == "*" then
+                clearBuffer()
+                mode = "literal"
+            end
+        end
+        if char == '"' then
+            if mode == "string" then
+                
+            else
+        end
     end
 
     return literal, def
+end
+
+local function clearBuffer()
+
 end
 
 return lexer
